99a4752b36375f5935e92a63ff7b183906867756
Welinton <welinton@realizati.com.br>
2019-08-16 23:26:43 -0300

    First Commit

---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <AppProfile />
+</template>
+
+<script>
+import AppProfile from './profile'
+
+export default {
+  components: {
+    AppProfile
+  }
+}
+</script>
+
+<style>
+body {
+  background-image: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%);
+}
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: BlinkMacSystemFont, -apple-system, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    'Helvetica', 'Arial', sans-serif;
+  display: block;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+  padding: 30px 30px 0px 30px;
+  font-weight: 700;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+}
+
+.links {
+  padding-top: 15px;
+}
+
+.footer {
+  font-size: 18px;
+}
+</style>
---
+++ pages/profile.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="columns">
+    <div class="column">
+      <div class="container">
+        <section class="section">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">
+                Welinton Subtil
+              </p>
+              <p class="subtitle">
+                Developer Full-stack
+              </p>
+              <div class="content">
+                <ul>
+                  <li>
+                    Javascript
+                    <i class="fa fa-heart" aria-hidden="true"></i>
+                  </li>
+                  <li>NodeJS</li>
+                  <li>PostgreSQL</li>
+                  <li>VueJS</li>
+                  <li>...</li>
+                </ul>
+              </div>
+            </div>
+            <footer class="card-footer">
+              <p class="card-footer-item">
+                <span>
+                  <i class="fa fa-github" aria-hidden="true"></i>
+                  <a href="https://github.com/wosubtil" target="_blank"
+                    >Github</a
+                  >
+                </span>
+              </p>
+              <p class="card-footer-item">
+                <span>
+                  <i class="fa fa-twitter" aria-hidden="true"></i>
+                  <a href="https://twitter.com/wosubtil" target="_blank"
+                    >Twitter</a
+                  >
+                </span>
+              </p>
+              <p class="card-footer-item">
+                <span>
+                  <i class="fa fa-linkedin" aria-hidden="true"></i>
+                  <a
+                    href="https://www.linkedin.com/in/welinton-subtil-68397639/"
+                    target="_blank"
+                    >Linkedin</a
+                  >
+                </span>
+              </p>
+            </footer>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppProfile'
+}
+</script>
+<style scoped>
+.content ul {
+  list-style-type: none;
+  margin-left: 0px;
+  margin-top: 0px;
+}
+</style>
